dcd78fdfc6f9e4d02606df6239b8c43a90804eec
Muhammad Hadi Naeem <hadinaeem365@gmail.com>
2024-09-16 23:23:35 +0500

    Update ThirdPersonShooterController.cs

--- Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
+++ Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonShooterController.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
 }
